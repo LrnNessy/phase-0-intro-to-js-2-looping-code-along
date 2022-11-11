b98bdf9cd774a4284625a4c3b783aa63f2fa5b7a
Lauren Shaughnessy <lrn.nessy@gmail.com>
2022-11-11 17:14:37 -0500

    commit tree 6a0e55f8f64bb142b872e728139f6031a28df6a0

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+const names= ['Guadalupe', 'Ollie', 'Aki'];
+
+function writeCards(names) {
+let messages = [];
+  for (let i = 0;  i < names.length; i++) {
+  messages.push (`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+     }
+ return messages;
+ }
+
+
+
+
+function countDown(){
+  let countdown = 10;
+  while (countdown >= 0){
+    console.log(countdown)
+    countdown--;
+  }
+}
